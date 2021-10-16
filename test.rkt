39ef9bee2371298644e9f9ba4ce6f6f976083d6c
Moritz Goeckel <moritz.goeckel@gmail.com>
2021-10-16 12:51:06 +0200

    Binary search

--- test.rkt
+++ test.rkt
@@ -15,9 +15,29 @@
 ;(append l (list 1))
 
 
-; Sorting
+; # Sorting
 
-; Search
+; # Binary Search
+(define searchImpl (lambda (input pred hi lo)
+                           (if (= hi lo)
+                               (let
+                                   ((elem (list-ref input hi)))
+                                   (if (pred elem)
+                                       elem
+                                       null))
+                               (letrec
+                                   ((mid (+ (floor (/ (- hi lo) 2)) lo))
+                                    (elem (list-ref input mid)))
+                                   (if (pred elem)
+                                       (searchImpl input pred mid lo)
+                                       (searchImpl input pred hi (+ mid 1))
+                                   )))))
+
+(define search
+  (lambda (input pred)
+          (searchImpl input pred (- (length input) 1) 0)))
+
+(search (list 0 1 2 3 4 5 6 7) (lambda (x) (> x 9)))
 
 ; Subsets
 (define subsetsImpl
@@ -35,5 +55,17 @@
 (define subsets
   (lambda (input) (subsetsImpl 0 input (list))))
 
-(subsets (list 1 2 3))
+;(display (subsets (list 1 2 3)))
+
+; # Permutations
+
+; # Backtracking
+
+; # Pruning
+
+; # Meet in the middle
+
+; # Greedy
+
+; # Dynamic Programming
 
